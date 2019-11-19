4a2275e4020c39880164a2118fe5182602b296c5
Klassen.Olga <lefelog@mail.ru>
2019-11-19 18:11:49 +0100

    created register.php, index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php require_once "views/register.php"?>
+<!DOCTYPE html>
+<html lang="de">
+
+<header>
+    <title>First Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</header>
+
+<body>
+
+    
+        
+
+    </form>
+</body>
+<html>
