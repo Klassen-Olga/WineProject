29f26b59c60ba2fdc7ff4ceb079da05f0dfd10ee
BenjaminS01 <bswarovsky@gmx.net>
2019-11-25 18:08:27 +0100

    WineProjectBenjaminSwarovsky created

---
+++ WineProjectBenjaminSwarovsky/index.php
@@ -0,0 +1,24 @@
+
+<?php require_once "config/database.php"?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<header>
+    <title>First Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</header>
+
+<body>
+<?php include "views/site.php"?>
+<?php include "views/start.php"?>
+
+
+   
+</body>
+
+<footer>
+<?php include "views/pages/imprint.php"?>
+</footer>
+</html>
