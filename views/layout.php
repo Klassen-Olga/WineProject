b2ac675f468145732d6514b11c72d2badf508d42
Klassen.Olga <lefelog@mail.ru>
2020-01-12 15:03:58 +0100

    responsive navi done

--- views/layout.php
+++ views/layout.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/styles/style2.css" type="text/css">
+    <link rel="stylesheet" href="assets/styles/style.css" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="assets/js/script1.js"></script>
+    <script src="assets/js/script.js"></script>
 </head>
 <body>
 <header>
