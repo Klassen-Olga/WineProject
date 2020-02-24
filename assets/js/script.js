9e7929cdc6bb3538fb50a406e7372d6ee5897272
BenjaminS01 <bswarovsky@gmx.net>
2020-02-24 12:59:22 +0100

    loginLogout PayPal Impressum

--- assets/js/script.js
+++ assets/js/script.js
@@ -312,23 +312,3 @@ function filterFunction() {
 function newWindow() {
     window.open("https://www.paypal.com/sg/signin");
 }
-
-/*$(document).ready(function(){
-    $(window).scroll(function(){
-        var lastID = $('.load-more').attr('lastID');
-        if(($(window).scrollTop() == $(document).height() - $(window).height()) && (lastID != 0)){
-            $.ajax({
-                type:'POST',
-                url:'ajax_more.php',
-                data:'prodId='+lastID,
-                beforeSend:function(){
-                    $('.load-more').show();
-                },
-                success:function(html){
-                    $('.load-more').remove();
-                    $('#postList').append(html);
-                }
-            });
-        }
-    });
-});*/
