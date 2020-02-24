05d0c81f684758ddfa95468616e2cafa254a3903
Klassen.Olga <lefelog@mail.ru>
2020-02-24 15:21:27 +0100

    readme

--- index.php
+++ index.php
@@ -2,18 +2,7 @@
 ini_set('session.cookie_lifetime', 0);
 session_start();
 require_once 'config/imports.php';
-/*
-$controllerName=$_GET['c'] ?? 'products';
-$actionName=$_GET['a'] ?? 'allProducts';*/
-/*$_GET['page']=1;
-$_GET['minPrice']=0.1;
-$_GET['maxPrice']=10;
-$_GET['region']="Germany";*/
-/*$_GET['year']=2019;*/
-/*$controllerName=$_GET['c'] ?? 'products';
-$actionName=$_GET['a'] ?? 'theProduct';
-/*$_GET['page']=1;*/
-/*$_GET['i']='.sdfc.sakejv';*/
+
 $controllerName=$_GET['c'] ?? 'pages';
 $actionName=$_GET['a'] ?? 'start';
 
